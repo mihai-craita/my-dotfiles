3dc0ce5a982f1f825aec9d186903d9d9cf2be016
Mihai Craita <mcraita@gmail.com>
2022-10-12 22:11:16 +0300

    Add completion sources for cmdline and path

--- nvim/.config/nvim/lua/plugins/packer.lua
+++ nvim/.config/nvim/lua/plugins/packer.lua
@@ -20,6 +20,8 @@ return require('packer').startup(function(use)
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
   use 'sheerun/vim-polyglot'
   use 'junegunn/vim-easy-align'
   use 'mhinz/vim-startify'

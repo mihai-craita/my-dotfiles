a51910e7b61898d57b376db2bfc4028a0c3cfe74
Mihai Craita <mcraita@gmail.com>
2022-10-15 08:53:28 +0300

    Load emmylua-nvim only in lua ft

--- nvim/.config/nvim/lua/plugins/packer.lua
+++ nvim/.config/nvim/lua/plugins/packer.lua
@@ -16,13 +16,14 @@ return require('packer').startup(function(use)
   use { 'scrooloose/nerdtree', cmd = { 'NERDTree', 'NERDTreeFind', 'NERDTreeFocus', 'NERDTreeToggle' } }
   use 'tpope/vim-commentary'
   use 'tpope/vim-fugitive'
+
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-cmdline'
   use 'saadparwaiz1/cmp_luasnip'
   use 'hrsh7th/nvim-cmp'
-  use ({"L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*"})
+  use 'L3MON4D3/LuaSnip'
 
   -- snippets sources
   use 'rafamadriz/friendly-snippets'
@@ -33,7 +34,7 @@ return require('packer').startup(function(use)
   use 'mhinz/vim-startify'
 
   -- neovim completion library for lua language nvim library
-  use 'ii14/emmylua-nvim'
+  use { 'ii14/emmylua-nvim', ft = { 'lua' } }
 
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use 'nvim-lua/plenary.nvim'

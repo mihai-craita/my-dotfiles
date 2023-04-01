758ae1ae7150422c7ce1d69b6a79e56780bd2f3b
Mihai Craita <mcraita@gmail.com>
2023-04-01 09:51:53 +0300

    Lua zero plugin

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,61 +1,8 @@
--- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local on_attach = function(client, bufnr)
-    local opts = { buffer = bufnr, noremap = true, silent = true }
+-- set up LSPs with :Mason
+local lsp = require('lsp-zero').preset({})
 
-    vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-    vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    -- go to next error
-    vim.keymap.set("n", "<leader>e", vim.diagnostic.goto_next, opts)
-    --  vim.keymap.set("n", "<leader>i", "<cmd>Telescope lsp_implementations<CR>", opts)
-end
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({buffer = bufnr})
+end)
 
-require'lspconfig'.intelephense.setup {
-  capabilities = capabilities,
-  on_attach = on_attach,
-}
-
-require'lspconfig'.gopls.setup{
-  capabilities = capabilities,
-  on_attach = on_attach,
-}
-
-require'lspconfig'.rust_analyzer.setup{
-    cmd = { "rustup", "run", "stable", "rust-analyzer"},
-    capabilities = capabilities,
-    on_attach = on_attach,
-}
-
-require'lspconfig'.tsserver.setup{
-  capabilities = capabilities,
-  on_attach = on_attach,
-}
-
-require'lspconfig'.pyright.setup{
-    capabilities = capabilities,
-    on_attach = on_attach,
-}
-
-local runtime_path = vim.split(package.path, ";", {})
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
-require'lspconfig'.sumneko_lua.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-      Lua = {
-        runtime = { version = "LuaJIT", path = runtime_path, },
-        diagnostics = {
-          enable = true,
-          globals = { "vim" },
-        },
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = vim.api.nvim_get_runtime_file("", true),
-        },
-      }
-    }
-  })
+lsp.setup()
--- nvim/.config/nvim/lua/plugins/packer.lua
+++ nvim/.config/nvim/lua/plugins/packer.lua
@@ -2,80 +2,97 @@
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-  vim.cmd [[packadd packer.nvim]]
+    is_bootstrap = true
+    vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+    vim.cmd [[packadd packer.nvim]]
 end
 
 require('packer').startup(function(use)
-  -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
-  -- make sure to add this line to let packer manage itself
-  use 'wbthomason/packer.nvim'
+    -- My plugins here
+    -- use 'foo1/bar1.nvim'
+    -- use 'foo2/bar2.nvim'
+    -- make sure to add this line to let packer manage itself
+    use 'wbthomason/packer.nvim'
 
-  use { 'scrooloose/nerdtree', cmd = { 'NERDTree', 'NERDTreeFind', 'NERDTreeFocus', 'NERDTreeToggle' } }
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-fugitive'
+    use { 'scrooloose/nerdtree', cmd = { 'NERDTree', 'NERDTreeFind', 'NERDTreeFocus', 'NERDTreeToggle' } }
+    use 'tpope/vim-commentary'
+    use 'tpope/vim-fugitive'
 
-  use 'neovim/nvim-lspconfig'
+    -- use { -- Autocompletion
+    --   requires = { 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-cmdline', 'saadparwaiz1/cmp_luasnip' },
+    -- }
+    -- neovim completion library for lua language nvim library
+    use { 'ii14/emmylua-nvim', ft = { 'lua' }, after = 'nvim-cmp' }
 
-  use { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-cmdline', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-  }
-  -- neovim completion library for lua language nvim library
-  use { 'ii14/emmylua-nvim', ft = { 'lua' }, after = 'nvim-cmp' }
+    -- snippets sources
+    use 'rafamadriz/friendly-snippets'
+    use 'honza/vim-snippets'
 
-  -- snippets sources
-  use 'rafamadriz/friendly-snippets'
-  use 'honza/vim-snippets'
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            {'mfussenegger/nvim-dap'}, -- Optional
 
-  use 'junegunn/vim-easy-align'
-  use 'mhinz/vim-startify'
-  use 'jwalton512/vim-blade'
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    }
 
-  use { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-    end,
-  }
+    use 'junegunn/vim-easy-align'
+    use 'mhinz/vim-startify'
+    use 'jwalton512/vim-blade'
 
-  use { -- Additional text objects via treesitter
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    after = 'nvim-treesitter',
-  }
+    -- Highlight, edit, and navigate code
+    use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
-  -- Fuzzy Finder (files, lsp, etc)
-  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    run = 'make',
-    after = 'telescope.nvim'
-  }
+    use { -- Additional text objects via treesitter
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        after = 'nvim-treesitter',
+    }
 
-  -- show context up
-  use 'nvim-treesitter/nvim-treesitter-context'
+    -- Fuzzy Finder (files, lsp, etc)
+    use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+    use {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        run = 'make',
+        after = 'telescope.nvim'
+    }
 
-  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
+    -- show context up
+    use 'nvim-treesitter/nvim-treesitter-context'
 
-  -- colors packs, find more on http://vimcolors.com/
-  use 'ellisonleao/gruvbox.nvim'
-  use 'sainnhe/sonokai'
-  use 'mhartington/oceanic-next'
-  use 'savq/melange'
-  use 'ishan9299/nvim-solarized-lua'
-  use 'marko-cerovac/material.nvim'
-  use 'rose-pine/neovim'
+    use 'nvim-lualine/lualine.nvim' -- Fancier statusline
 
-  use 'kyazdani42/nvim-web-devicons' -- fonts
+    -- colors packs, find more on http://vimcolors.com/
+    use 'sainnhe/gruvbox-material'
+    use 'sainnhe/edge'
+    use 'sainnhe/everforest'
+    use 'sainnhe/sonokai'
+    use 'ellisonleao/gruvbox.nvim'
+    use 'mhartington/oceanic-next'
+    use 'savq/melange'
+    use 'ishan9299/nvim-solarized-lua'
+    use 'marko-cerovac/material.nvim'
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if is_bootstrap then
-    require('packer').sync()
-  end
+    use 'kyazdani42/nvim-web-devicons' -- fonts
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if is_bootstrap then
+        require('packer').sync()
+    end
 end)
 
 -- When we are bootstrapping a configuration, it doesn't
@@ -83,10 +100,10 @@ end)
 --
 -- You'll need to restart nvim, and then it will work.
 if is_bootstrap then
-  print '=================================='
-  print '    Plugins are being installed'
-  print '    Wait until Packer completes,'
-  print '       then restart nvim'
-  print '=================================='
-  return
+    print '=================================='
+    print '    Plugins are being installed'
+    print '    Wait until Packer completes,'
+    print '       then restart nvim'
+    print '=================================='
+    return
 end
